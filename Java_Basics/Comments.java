5f3d5934af100aef415c6f5210def264efbb7411
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:34:44 +0600

    Create Comments.java

---
+++ Java_Basics/Comments.java
@@ -0,0 +1,24 @@
+class Comments {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+
+        /*
+         * This is a
+         * multi-line comment
+         */
+
+        int a = 10;
+        int b = 20;
+
+        // Adding two numbers
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+    }
+}
+
+/*
+Output:
+Sum: 30
+*/
