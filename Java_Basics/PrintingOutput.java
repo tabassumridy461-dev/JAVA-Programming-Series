51de71c7ac78650c1ef969ac98832f68c4efa9a1
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 10:20:54 +0600

    Create PrintingOutput.java

---
+++ Java_Basics/PrintingOutput.java
@@ -0,0 +1,27 @@
+public class PrintingOutput {
+
+    public static void main(String[] args) {
+
+        // Print without moving to a new line
+        System.out.print("Hello ");
+
+        // Print and move to a new line
+        System.out.println("Java!");
+
+        // Print multiple lines
+        System.out.println("I am learning Java.");
+        System.out.println("This is my Java Programming Series.");
+
+        // Printing numbers
+        System.out.println(100);
+        System.out.println(25 + 10);
+
+    }
+}
+
+/*Output
+Hello Java!
+I am learning Java.
+This is my Java Programming Series.
+100
+35 */
