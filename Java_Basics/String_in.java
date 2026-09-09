6cbe7e5a31bd4db6fe75d173d9567c6cc774d012
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:36:31 +0600

    Add StringBasics class to demonstrate string methods

---
+++ Java_Basics/String_in.java
@@ -0,0 +1,24 @@
+class StringBasics {
+    public static void main(String[] args) {
+
+        String name = "Tabassum";
+        String university = "Daffodil University";
+
+        System.out.println("Name: " + name);
+        System.out.println("University: " + university);
+        System.out.println("Name Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First Character: " + name.charAt(0));
+    }
+}
+
+/*
+Output:
+Name: Tabassum
+University: Daffodil University
+Name Length: 8
+Uppercase: TABASSUM
+Lowercase: tabassum
+First Character: T
+*/
