fe341145245b02e6141ea4b1f1ef0659623c1fdb
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:33:00 +0600

    Add LogicalOperators class demonstrating logical ops

---
+++ Java_Basics/Logical_Operators.java
@@ -0,0 +1,20 @@
+class LogicalOperators {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("a && b: " + (a && b));
+        System.out.println("a || b: " + (a || b));
+        System.out.println("!a: " + (!a));
+        System.out.println("!b: " + (!b));
+    }
+}
+
+/*
+Output:
+a && b: false
+a || b: true
+!a: false
+!b: true
+*/
