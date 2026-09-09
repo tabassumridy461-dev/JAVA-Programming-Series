34fe4ba75a2b0e963d42e4b47e6142f49fb3c89a
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:35:17 +0600

    Add EscapeSequences class to demonstrate escape sequences

---
+++ Java_Basics/Escape_Sequences.java
@@ -0,0 +1,18 @@
+class EscapeSequences {
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nWorld");
+        System.out.println("Java\tProgramming");
+        System.out.println("He said, \"Java is easy!\"");
+        System.out.println("This is a backslash: \\");
+    }
+}
+
+/*
+Output:
+Hello
+World
+Java    Programming
+He said, "Java is easy!"
+This is a backslash: \
+*/
