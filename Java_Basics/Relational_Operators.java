0d4aeab059e09f1a9a672723a808f8dce0665dab
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:32:27 +0600

    Add RelationalOperators class to demonstrate comparisons

---
+++ Java_Basics/Relational_Operators.java
@@ -0,0 +1,24 @@
+class RelationalOperators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+    }
+}
+
+/*
+Output:
+a == b: false
+a != b: true
+a > b: false
+a < b: true
+a >= b: false
+a <= b: true
+*/
