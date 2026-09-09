0042ca631bf9e1ece9c2e38dea2f8293c2dbd90f
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 10:33:06 +0600

    Create Variables class to display user information
    This Java program defines a class 'Variables' that initializes and prints user details including name, age, CGPA, grade, and student status.

---
+++ Java_Basics/Variables.java
@@ -0,0 +1,24 @@
+class Variables {
+    public static void main(String[] args) {
+
+        String name = "Tabassum";
+        int age = 22;
+        double cgpa = 3.83;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
+
+Output:
+
+Name: Tabassum
+Age: 22
+CGPA: 3.83
+Grade: A
+Student: true
