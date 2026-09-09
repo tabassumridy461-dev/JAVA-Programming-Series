665a150af7b99d84c76fb368e70e796ab1676f14
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 10:45:31 +0600

    Create DataTypes.java

---
+++ Java_Basics/DataTypes.java
@@ -0,0 +1,35 @@
+class DataTypes {
+    public static void main(String[] args) {
+
+        byte smallNumber = 100;
+        short shortNumber = 10000;
+        int age = 22;
+        long population = 170000000L;
+
+        float price = 99.99f;
+        double cgpa = 3.83;
+
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short: " + shortNumber);
+        System.out.println("Int: " + age);
+        System.out.println("Long: " + population);
+        System.out.println("Float: " + price);
+        System.out.println("Double: " + cgpa);
+        System.out.println("Char: " + grade);
+        System.out.println("Boolean: " + isStudent);
+    }
+}
+
+Output:
+
+Byte: 100
+Short: 10000
+Int: 22
+Long: 170000000
+Float: 99.99
+Double: 3.83
+Char: A
+Boolean: true
