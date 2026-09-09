6e270f5bfd30c12a9c1078631781f3165186973d
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:34:10 +0600

    Create Constants.java

---
+++ Java_Basics/Constants.java
@@ -0,0 +1,16 @@
+class Constants {
+    public static void main(String[] args) {
+
+        final double PI = 3.14159;
+        final int DAYS_IN_WEEK = 7;
+
+        System.out.println("Value of PI: " + PI);
+        System.out.println("Days in a week: " + DAYS_IN_WEEK);
+    }
+}
+
+/*
+Output:
+Value of PI: 3.14159
+Days in a week: 7
+*/
