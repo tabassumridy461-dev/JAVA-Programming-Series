0e9ab840fcf681761605eaf99fa3352e49174645
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:37:04 +0600

    Create Basic_Calculator.java

---
+++ Java_Basics/Basic_Calculator.java
@@ -0,0 +1,25 @@
+class BasicCalculator {
+    public static void main(String[] args) {
+
+        double num1 = 20;
+        double num2 = 5;
+
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num1 / num2;
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+    }
+}
+
+/*
+Output:
+Addition: 25.0
+Subtraction: 15.0
+Multiplication: 100.0
+Division: 4.0
+*/
