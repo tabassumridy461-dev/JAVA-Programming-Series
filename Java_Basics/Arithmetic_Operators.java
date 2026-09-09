0a9fe9cc643599bcf7e0b4f67a4cf3232cd125e0
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-09 21:31:13 +0600

    Change class declaration to package-private

---
+++ Java_Basics/Arithmetic_Operators.java
@@ -0,0 +1,22 @@
+class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 6;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
+
+/*
+Output:
+Addition: 26
+Subtraction: 14
+Multiplication: 120
+Division: 3
+Modulus: 2
+*/
