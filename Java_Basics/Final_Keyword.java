2397ec11e0315bf85a0400671d48252474adbc9f
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-10 13:24:42 +0600

    Add FinalKeyword class demonstrating final variables

---
+++ Java_Basics/Final_Keyword.java
@@ -0,0 +1,22 @@
+public class FinalKeyword {
+
+    public static void main(String[] args) {
+
+        final int DAYS_IN_WEEK = 7;
+        final double PI = 3.14159;
+
+        System.out.println("Days in a Week: " + DAYS_IN_WEEK);
+        System.out.println("Value of PI: " + PI);
+
+        final String UNIVERSITY = "Daffodil International University";
+
+        System.out.println("University: " + UNIVERSITY);
+    }
+
+    /*
+    Output:
+    Days in a Week: 7
+    Value of PI: 3.14159
+    University: Daffodil International University
+    */
+}
