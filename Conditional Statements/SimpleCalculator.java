092ace1c1e82c3bb49eca9f2277c9e2a08f03c1c
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-11 21:57:22 +0600

    Create SimpleCalculator.java

---
+++ Conditional Statements/SimpleCalculator.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                System.out.println("Result: " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result: " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result: " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println("Result: " + (num1 / num2));
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator.");
+        }
+
+        input.close();
+    }
+}
+
+/*
+Output:
+Enter first number: 20
+Enter operator (+, -, *, /): +
+Enter second number: 10
+Result: 30.0
+*/
