d872f4851a4dffe87bd13bb2449f8bf9ebb8ae02
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-11 21:55:18 +0600

    Add CheckNumberType to determine number positivity

---
+++ Conditional Statements/CheckNumberType.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckNumberType {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        input.close();
+    }
+}
+
+/*
+Output:
+Enter a number: -8
+The number is negative.
+*/
