b30f04c0ae4bd3ea5de0c755412539b411467d52
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:07:17 +0600

    Add BasicWhileLoop class with while loop example
    Implement a basic while loop that prints numbers from 1 to 5.

---
+++ Loops/BasicWhileLoop.java
@@ -0,0 +1,18 @@
+public class BasicWhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+
+        // Output:
+        // 1
+        // 2
+        // 3
+        // 4
+        // 5
+    }
+}
