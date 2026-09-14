b679a4eafb17c100f2f7bdb96e019f1225de6c2d
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:05:50 +0600

    Add EvenNumbers class to print even numbers
    Implement EvenNumbers class to print even numbers from 2 to 10.

---
+++ Loops/EvenNumbers.java
@@ -0,0 +1,15 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+
+        // Output:
+        // 2
+        // 4
+        // 6
+        // 8
+        // 10
+    }
+}
