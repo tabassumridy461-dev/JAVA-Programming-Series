5c586733e0adce6b7fdfe8c6beb4c38a68e3d96a
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:08:20 +0600

    Add ReverseCounting class to print numbers in reverse

---
+++ Loops/ReverseCounting.java
@@ -0,0 +1,15 @@
+public class ReverseCounting {
+    public static void main(String[] args) {
+
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        // Output:
+        // 5
+        // 4
+        // 3
+        // 2
+        // 1
+    }
+}
