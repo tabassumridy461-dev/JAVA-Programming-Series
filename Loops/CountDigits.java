7a625be09fe2f3de9091b57273e5c843557428d8
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:14:11 +0600

    Add CountDigits class to count digits in a number

---
+++ Loops/CountDigits.java
@@ -0,0 +1,17 @@
+public class CountDigits {
+    public static void main(String[] args) {
+
+        int number = 12345;
+        int count = 0;
+
+        while (number > 0) {
+            number /= 10;
+            count++;
+        }
+
+        System.out.println("Number of digits = " + count);
+
+        // Output:
+        // Number of digits = 5
+    }
+}
