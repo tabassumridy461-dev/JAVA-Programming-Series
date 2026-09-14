09671ce06a6962536bf2b97c9ed06356e8233aac
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:06:18 +0600

    Add OddNumbers class to print odd numbers 1 to 10

---
+++ Loops/OddNumbers.java
@@ -0,0 +1,15 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+
+        // Output:
+        // 1
+        // 3
+        // 5
+        // 7
+        // 9
+    }
+}
