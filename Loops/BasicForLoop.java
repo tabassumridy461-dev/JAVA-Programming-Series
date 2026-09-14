5724802b39e356c745e2b99cd223f019bb905e78
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:05:17 +0600

    Create BasicForLoop.java

---
+++ Loops/BasicForLoop.java
@@ -0,0 +1,15 @@
+public class BasicForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // Output:
+        // 1
+        // 2
+        // 3
+        // 4
+        // 5
+    }
+}
