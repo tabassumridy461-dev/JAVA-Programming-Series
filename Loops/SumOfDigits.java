afdf689aae8c45fe569ab03e96302d26ffdfddc6
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-14 22:13:27 +0600

    Add SumOfDigits program to calculate digit sum

---
+++ Loops/SumOfDigits.java
@@ -0,0 +1,18 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        int number = 12345;
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+
+        // Output:
+        // Sum of digits = 15
+    }
+}
