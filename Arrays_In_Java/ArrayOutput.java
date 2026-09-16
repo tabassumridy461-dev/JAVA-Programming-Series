fba1874224e6c25ea48f4c4b146d35ac50368988
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-16 20:14:51 +0600

    Add ArrayOutput class to print array elements

--- Arrays_In_Java/ArrayOutput.java
+++ Arrays_In_Java/ArrayOutput.java
@@ -1,1 +1,16 @@
+// ArrayOutput.java
 
+public class ArrayOutput {
+
+    public static void main(String[] args) {
+
+        // Create and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print all elements using a for loop
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
