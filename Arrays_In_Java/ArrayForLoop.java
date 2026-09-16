7ceb6d9eaacca5e425b7faee16ad268639c7feba
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-16 20:22:10 +0600

    Create ArrayForLoop.java

---
+++ Arrays_In_Java/ArrayForLoop.java
@@ -0,0 +1,17 @@
+// ArrayForLoop.java
+
+public class ArrayForLoop {
+
+    public static void main(String[] args) {
+
+        // Create and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Use for loop to visit every element
+        for (int i = 0; i < numbers.length; i++) {
+
+            // Print the current element
+            System.out.println(numbers[i]);
+        }
+    }
+}
