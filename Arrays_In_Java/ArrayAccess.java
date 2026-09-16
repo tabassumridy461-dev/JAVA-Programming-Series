219c3c60309bd9a3c758cfa3fa9df8642154caf1
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-16 20:12:21 +0600

    Add ArrayAccess.java to demonstrate array access

---
+++ Arrays_In_Java/ArrayAccess.java
@@ -0,0 +1,17 @@
+// ArrayAccess.java
+
+public class ArrayAccess {
+
+    public static void main(String[] args) {
+
+        // Create and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Access individual elements
+        System.out.println("First element: " + numbers[0]);
+        System.out.println("Second element: " + numbers[1]);
+        System.out.println("Third element: " + numbers[2]);
+        System.out.println("Fourth element: " + numbers[3]);
+        System.out.println("Fifth element: " + numbers[4]);
+    }
+}
