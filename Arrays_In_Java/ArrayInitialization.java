3b520b8b29683b27c313c7223750bdd6a609f150
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-16 20:10:12 +0600

    Add ArrayInitialization.java for array example

---
+++ Arrays_In_Java/ArrayInitialization.java
@@ -0,0 +1,24 @@
+// ArrayInitialization.java
+
+public class ArrayInitialization {
+
+    public static void main(String[] args) {
+
+        // Declare an array and specify its size
+        int[] numbers = new int[5];
+
+        // Store values in the array
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        // Print the values
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers[3]);
+        System.out.println(numbers[4]);
+    }
+}
