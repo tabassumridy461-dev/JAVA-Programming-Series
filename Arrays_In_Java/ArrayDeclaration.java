5b794993891086f69a34cd7b099b294c37fd36ca
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-16 20:09:36 +0600

    Create ArrayDeclaration.java for array examples
    Added a Java class to demonstrate array declarations.

---
+++ Arrays_In_Java/ArrayDeclaration.java
@@ -0,0 +1,19 @@
+// ArrayDeclaration.java
+
+public class ArrayDeclaration {
+
+    public static void main(String[] args) {
+
+        // Declaring an integer array
+        int[] numbers;
+
+        // Declaring a String array
+        String[] names;
+
+        // Declaring a double array
+        double[] prices;
+
+        // Printing a message
+        System.out.println("Arrays declared successfully.");
+    }
+}
