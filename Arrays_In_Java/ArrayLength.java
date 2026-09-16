1de78eccc5cc39a45eb93db612d13550c7876c68
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-16 20:13:32 +0600

    Add ArrayLength.java to demonstrate array length

---
+++ Arrays_In_Java/ArrayLength.java
@@ -0,0 +1,16 @@
+// ArrayLength.java
+
+public class ArrayLength {
+
+    public static void main(String[] args) {
+
+        // Create and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Find the length of the array
+        int length = numbers.length;
+
+        // Print the length
+        System.out.println("Array length: " + length);
+    }
+}
