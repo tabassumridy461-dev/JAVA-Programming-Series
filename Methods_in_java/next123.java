98ae52d098a02fc09a76f84d8947a451c449478f
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:14:10 +0600

    Add next123.java with directory structure

---
+++ Methods_in_java/next123.java
@@ -0,0 +1,71 @@
+Methods_in_Java
+│
+├── 01_Method_Basics
+│   ├── SimpleMethod.java
+│   ├── MethodCalling.java
+│   ├── MethodWithParameters.java
+│   └── MethodWithReturnValue.java
+│
+├── 02_Method_Types
+│   ├── NoParameterNoReturn.java
+│   ├── ParameterNoReturn.java
+│   ├── NoParameterWithReturn.java
+│   └── ParameterWithReturn.java
+│
+├── 03_Method_Overloading
+│   ├── MethodOverloading.java
+│   ├── DifferentParameters.java
+│   └── DifferentDataTypes.java
+│
+├── 04_Static_Method
+│   ├── StaticMethod.java
+│   └── StaticVsNonStatic.java
+│
+├── 05_Instance_Method
+│   ├── InstanceMethod.java
+│   └── ObjectMethodCall.java
+│
+├── 06_Parameter_Passing
+│   ├── PassByValue.java
+│   └── PassingObjects.java
+│
+├── 07_Return_Statement
+│   ├── ReturnInt.java
+│   ├── ReturnString.java
+│   └── ReturnObject.java
+│
+├── 08_Recursive_Method
+│   ├── Factorial.java
+│   ├── Fibonacci.java
+│   └── SumOfNumbers.java
+│
+├── 09_Access_Modifiers
+│   ├── PublicMethod.java
+│   ├── PrivateMethod.java
+│   └── ProtectedMethod.java
+│
+├── 10_Method_With_Array
+│   ├── PassArray.java
+│   ├── ReturnArray.java
+│   └── FindMaxUsingMethod.java
+│
+├── 11_Method_With_String
+│   ├── StringMethod.java
+│   ├── CountVowels.java
+│   └── ReverseString.java
+│
+├── 12_Method_With_Object
+│   ├── PassObject.java
+│   └── ReturnObject.java
+│
+├── 13_Real_Life_Examples
+│   ├── Calculator.java
+│   ├── StudentResult.java
+│   ├── BankAccount.java
+│   └── EmployeeSalary.java
+│
+└── 14_Practice
+    ├── Practice01.java
+    ├── Practice02.java
+    ├── Practice03.java
+    └── Practice04.java
