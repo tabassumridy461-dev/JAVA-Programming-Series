1cb904a7739f385dc1f7b0a38a217d32b4df6ced
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:11:12 +0600

    Add ReturnString class to return a name

---
+++ Methods_in_java/ReturnString.java
@@ -0,0 +1,13 @@
+public class ReturnString {
+
+    static String getName() {
+        return "Tabassum";
+    }
+
+    public static void main(String[] args) {
+
+        String name = getName();
+
+        System.out.println("Name = " + name);
+    }
+}
