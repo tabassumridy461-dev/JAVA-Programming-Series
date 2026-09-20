887cdf52627f8fdfa67dd27209f89befacc6d58d
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:07:14 +0600

    Add StaticMethod class with a static display method

---
+++ Methods_in_java/StaticMethod.java
@@ -0,0 +1,11 @@
+public class StaticMethod {
+
+    static void display() {
+        System.out.println("This is a static method.");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+    }
+}
