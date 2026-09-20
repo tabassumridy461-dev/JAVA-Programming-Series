44eecbff3521753d24b898efbc515ea74b65db02
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:01:13 +0600

    Add SimpleMethod class with hello method

---
+++ Methods_in_java/SimpleMethod.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+
+    static void sayHello() {
+        System.out.println("Hello Java!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+}
