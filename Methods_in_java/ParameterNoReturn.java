b807c0cdd5fcc6be9b711d1f6ff98f6d431ade18
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:04:02 +0600

    Add ParameterNoReturn class with greet method

---
+++ Methods_in_java/ParameterNoReturn.java
@@ -0,0 +1,10 @@
+public class ParameterNoReturn {
+
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("Tabassum");
+    }
+}
