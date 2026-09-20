48c1da051deee3d6c9c87de6f33b0641818e5cbc
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:10:43 +0600

    Add ReturnInt class to calculate square of a number

---
+++ Methods_in_java/ReturnInt.java
@@ -0,0 +1,13 @@
+public class ReturnInt {
+
+    static int square(int number) {
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+
+        int result = square(5);
+
+        System.out.println("Square = " + result);
+    }
+}
