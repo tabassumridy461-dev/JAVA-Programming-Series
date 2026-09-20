c286ee6cae226972feae0421ab24a52e20c1358a
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:03:24 +0600

    Create NoParameterNoReturn.java

---
+++ Methods_in_java/NoParameterNoReturn.java
@@ -0,0 +1,10 @@
+public class NoParameterNoReturn {
+
+    static void display() {
+        System.out.println("Hello Java!");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
