31abd46548b2462e3b47862452518842753988dd
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:05:04 +0600

    Add ParameterWithReturn class to calculate sum

---
+++ Methods_in_java/ParameterWithReturn.java
@@ -0,0 +1,13 @@
+public class ParameterWithReturn {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(10, 20);
+
+        System.out.println("Sum = " + result);
+    }
+}
