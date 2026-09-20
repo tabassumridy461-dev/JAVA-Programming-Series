156ee245cf5739bb30d7281e059d88f7c0ed0161
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-20 11:02:15 +0600

    Add MethodWithParameters class with greeting method

---
+++ Methods_in_java/MethodWithParameters.java
@@ -0,0 +1,12 @@
+public class MethodWithParameters {
+
+    static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+
+        greet("Tabassum");
+        greet("Rahim");
+    }
+}
