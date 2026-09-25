1eeccf6acf95c9aac43f3087fab7d475b3a63bcc
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:20:57 +0600

    Implement method overloading for addition with different types

---
+++ Methods_in_java/Method_Overloading/DifferentDataTypes.java
@@ -0,0 +1,16 @@
+public class DifferentDataTypes {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(add(10, 20));
+        System.out.println(add(10.5, 20.5));
+    }
+}
