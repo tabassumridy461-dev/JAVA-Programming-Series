391c575a49ef544657935e5e6df0505da90ea2c6
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:17:57 +0600

    Add ParameterNoReturn class to display a number

---
+++ Methods_in_java/Method_Types/ParameterNoReturn.java
@@ -0,0 +1,11 @@
+public class ParameterNoReturn {
+
+    static void showNumber(int number) {
+        System.out.println("Number: " + number);
+    }
+
+    public static void main(String[] args) {
+
+        showNumber(50);
+    }
+}
