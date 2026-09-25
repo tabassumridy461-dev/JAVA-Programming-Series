7cc052043e54f806d347c2fdeed387231fb5c522
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:19:08 +0600

    Add ParameterWithReturn class with multiply method

---
+++ Methods_in_java/Method_Types/ParameterWithReturn.java
@@ -0,0 +1,13 @@
+public class ParameterWithReturn {
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        int result = multiply(5, 4);
+
+        System.out.println("Result = " + result);
+    }
+}
