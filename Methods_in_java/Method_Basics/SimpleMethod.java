88dcab6646221946f1fb0973245a880002036b56
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:14:01 +0600

    Create SimpleMethod.java

---
+++ Methods_in_java/Method_Basics/SimpleMethod.java
@@ -0,0 +1,10 @@
+public class SimpleMethod {
+
+    static void greet() {
+        System.out.println("Hello, welcome to Java Methods!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
