31f897a2ab54d4efe12aec2f249f8dcb572539d7
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:15:29 +0600

    Add MethodWithParameters class with student info method

---
+++ Methods_in_java/Method_Basics/MethodWithParameters.java
@@ -0,0 +1,12 @@
+public class MethodWithParameters {
+
+    static void studentInfo(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+
+        studentInfo("Rahim", 20);
+    }
+}
