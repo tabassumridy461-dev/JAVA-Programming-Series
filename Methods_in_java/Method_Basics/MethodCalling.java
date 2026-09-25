fda5b52a6ff4a7748632f4c4bb4d2093c6dc38eb
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:14:47 +0600

    Add MethodCalling class with two static methods

---
+++ Methods_in_java/Method_Basics/MethodCalling.java
@@ -0,0 +1,16 @@
+public class MethodCalling {
+
+    static void firstMethod() {
+        System.out.println("This is the first method.");
+    }
+
+    static void secondMethod() {
+        System.out.println("This is the second method.");
+    }
+
+    public static void main(String[] args) {
+
+        firstMethod();
+        secondMethod();
+    }
+}
