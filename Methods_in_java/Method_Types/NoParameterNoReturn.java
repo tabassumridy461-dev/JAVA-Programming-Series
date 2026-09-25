dc027573ab3970a3867d9ddb0e05b8c3844c18c0
Tabassum Ridy <tabassumridy461@gmail.com>
2026-09-25 23:17:10 +0600

    Add NoParameterNoReturn class with display method

---
+++ Methods_in_java/Method_Types/NoParameterNoReturn.java
@@ -0,0 +1,11 @@
+public class NoParameterNoReturn {
+
+    static void display() {
+        System.out.println("Hello Java!");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+    }
+}
